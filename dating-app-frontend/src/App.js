4bd78d926935ad7a3ec616b25a30564456b40e96
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2022-11-14 17:00:02 -0800

    finished the front end up

--- dating-app-frontend/src/App.js
+++ dating-app-frontend/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import DatingCards from "./components/DatingCards.js";
+import SwipeButtons from "./components/SwipeButtons";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <DatingCards />
+      <SwipeButtons />
     </div>
   );
 }
