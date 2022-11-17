41a40afd934d16d9b2334442e91db73cab1bd5dc
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2022-11-16 20:44:42 -0800

    adjusted backend. added db config/env files

--- dating-app-backend/server.js
+++ dating-app-backend/server.js
@@ -1,14 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8000;
-
+const connection_url = process.env.URL;
 //Middleware
 
 //DB Config
-
+// mongoose.connect(connection_url, {
+//   useNewUrlParser: true,
+//   useCreateIndex: true,
+//   useUnifiedTopology: true,
+// });
 //API Endpoints
 
 app.get("/", (req, res) => res.status(200).send("Hello World"));
