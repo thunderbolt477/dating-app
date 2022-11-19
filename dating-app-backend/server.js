428553fbabdd6b367acaba685cee249e74434fe0
Timothy Balilo <85888459+thunderbolt477@users.noreply.github.com>
2022-11-18 20:25:26 -0800

    added mongodb and server code

--- dating-app-backend/server.js
+++ dating-app-backend/server.js
@@ -1,23 +1,45 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import Cards from "./dbCards.js";
+import Cors from "cors";
 dotenv.config();
 
 // App Config
 const app = express();
 const port = process.env.PORT || 8000;
-const connection_url = process.env.URL;
+const uri = process.env.URL;
 //Middleware
+app.use(express.json());
+app.use(Cors());
 
 //DB Config
-// mongoose.connect(connection_url, {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true,
-// });
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 //API Endpoints
 
 app.get("/", (req, res) => res.status(200).send("Hello World"));
+
+app.post("/dating/cards", (req, res) => {
+  const dbCard = req.body;
+  Cards.create(dbCard, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.get("/dating/cards", (req, res) => {
+  Cards.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
 
 //Listener
 app.listen(port, () => console.log(`listeing on ${port}`));
